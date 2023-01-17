d28f6ebfbfec5645d3f53c7b7714cbcefd6c9023
Julián <j.gakusei11@gmail.com>
2023-01-17 17:55:20 -0300

    TP Done

--- ChauMundo/Program.cs
+++ ChauMundo/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HolaMundo
+namespace ChauMundo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo!");
+            Console.ReadKey();
+            Console.WriteLine("Chau mundo");
             Console.Read();
         }
     }
